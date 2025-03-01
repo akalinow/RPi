47608fef7626be77d5faa83e2205845d2d98994a
Artur Kalinowski <akalinows@gmail.com>
2025-03-01 13:15:36 +0100

    * some updates

--- FaceFollow/face_follow.py
+++ FaceFollow/face_follow.py
@@ -1,4 +1,4 @@
-import time
+import time, random
 
 from Camera import Camera
 from Detector import Detector
@@ -13,6 +13,7 @@ from termcolor import colored
 
 #TensorFlow
 import tensorflow as tf
+import keras
 
 #Pandas
 import pandas as pd
@@ -20,35 +21,66 @@ import pandas as pd
 #Kaggle
 import kagglehub
 
+id_model = keras.saving.load_model("model_A_vs_W.keras")
+
+kaggle_model_path = "google/mobilenet-v3/tfLite/large-100-224-feature-vector"
+model_path = kagglehub.model_download(kaggle_model_path)
+print(colored("Model path:","blue"),model_path)
+interpreter = tf.lite.Interpreter(model_path=model_path+"/1.tflite")
+interpreter.allocate_tensors()
+###
+picam = Camera()
+detector = Detector('blaze_face_short_range.tflite')
+servos = Servos() 
+servos.setPosition((130,40))
+###
+
+###################################
+def sweepCamera():
+
+    initAngle = (130,40)
+
+    step = 15 ##degrees
+    for iStep in range(36):
+        iXStep = random.randint(-3,3)
+        iYStep = random.randint(-4,4)
+        testAngle = initAngle + step*np.array((iXStep,iYStep))
+        servos.setPosition(testAngle)
+        time.sleep(0.15)
+        image = picam.getRGBImage()
+        faces = detector.getVideoResponse(image)
+        print("\r",testAngle, end=' ',)
+        print("\r",len(faces), end=' ',)
+        if len(faces) > 0:
+           return testAngle
+
+    servos.setPosition(initAngle)
+    return None
 ###################################
 def test():
-
-    kaggle_model_path = "google/mobilenet-v3/tfLite/large-100-224-feature-vector"
-    model_path = kagglehub.model_download(kaggle_model_path)
-    print(colored("Model path:","blue"),model_path)
-    interpreter = tf.lite.Interpreter(model_path=model_path+"/1.tflite")
-    interpreter.allocate_tensors()
-
-    picam = Camera()
-    detector = Detector('blaze_face_short_range.tflite')
-    servos = Servos()
-    #servos.setPosition((90,85)) 
-    servos.setPosition((170,40))
 
     picamv2_fov = np.array((60, 30))
     deltaPos = np.zeros(2)
 
+    initialCamPos = sweepCamera()
+    if initialCamPos is None:
+        return
+    
+    servos.setPosition(initialCamPos)
     '''
     dummyRow = np.zeros((1280)),
     df = pd.DataFrame(data=dummyRow)
     df["label"] = np.full((1), -1)
+    df.to_parquet('df.parquet.gzip',compression='gzip') 
     '''
-
-    file_path = '/home/akalinow/scratch/RPi/FaceFollow/df.parquet.gzip'
+    file_path = '/home/akalinow/scratch/RPi/FaceFollow/df.parquet_Artur.gzip'
     df = pd.read_parquet(file_path)
-    df.drop(index=0, inplace=True)
-    df.reindex()
     features = df.drop(columns=['label'])
+    nExamples = len(df)
+
+    personCounter = {"Wojtek":0,
+                     "Artur":0,
+                    "Unknown":0}
     
     while True:
         start_time = time.time()
@@ -85,32 +117,30 @@ def test():
             interpreter.invoke()
             output = interpreter.get_tensor(output['index'])
 
-            distance = np.sum((output - features)**2, axis=1)
-            min_distance = np.min(distance)
-            min_dist_index = np.argmin(distance)
-            min_dist_label = df["label"].iloc[min_dist_index]
-            print("min. dist. and label:",min_distance, min_dist_label)
+            response = id_model(output)
+            if response<10:
+                print(colored("Artur!","blue"))
+                personCount["Artur"] +=1
+            elif response>10:
+                print(colored("Wojtek!","blue"))
+                 personCount["Wojtek"] +=1
+            else:
+                print(colored("Unknown!","blue"))
+                 personCount["Unknown"] +=1
 
-            max_distance = np.max(distance)
-            max_dist_index = np.argmax(distance)
-            max_dist_label = df["label"].iloc[min_dist_index]
-            print("max distance and label:", max_distance, max_dist_label)
-            new_item_label = min_dist_label
-            
-            if min_dist_label<0 or min_distance>200:
-                new_item_label = np.max(df["label"])+1
-
-            print("New item label:",new_item_label)
+            new_item_label = 0
             new_item_label = np.array(new_item_label).reshape((1,1))
             dataRow = np.concatenate( (output, new_item_label), axis=1)
-            df.loc[len(df)+10] = dataRow[0]
+            df.loc[len(df)+20] = dataRow[0]
             print("Number of examples:",len(df))
-            if len(df)>200:
+            if len(df)-nExamples>500:
                 break
             #######################################################
         else:
             cv2.imwrite("test_annotated.jpg", image)
 
+    lastPos = servos.getPosition()
+    print("Last camera position:",lastPos)
     print("")
     print(df.describe())
     df.to_parquet('df.parquet.gzip',
@@ -118,18 +148,3 @@ def test():
 
 ###################################
 test()
-
-'''
-face detector result: 
-DetectionResult(detections=[Detection(bounding_box=BoundingBox(origin_x=0, origin_y=168, width=366, height=366), 
-categories=[Category(index=0, score=0.5604583024978638, display_name=None, category_name=None)], 
-keypoints=[
-NormalizedKeypoint(x=0.003223732579499483, y=0.7284545302391052, label='', score=0.0), 
-NormalizedKeypoint(x=0.24945300817489624, y=0.6111683249473572, label='', score=0.0), 
-NormalizedKeypoint(x=0.1393018364906311, y=0.9198736548423767, label='', score=0.0), 
-NormalizedKeypoint(x=0.19469484686851501, y=1.0107040405273438, label='', score=0.0), 
-NormalizedKeypoint(x=-0.044375721365213394, y=0.7179324626922607, label='', score=0.0), 
-NormalizedKeypoint(x=0.44926655292510986, y=0.4869495630264282, label='', score=0.0)])])
-
-
-'''
